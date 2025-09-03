426667fce924b9bc39a153e8f591cffb0452faa8
理客_click <lickqaq@outlook.com>
2025-09-03 19:09:19 +0800

    feat(apps): 显示同步数据并移除冗余图片文件
    - 在 teleoperate_config 中将 display_data 设置为 True，以显示同步数据
    - 删除了不必要的图片文件 outputs/opencv__dev_video0.png

--- apps/sync_cam.py
+++ apps/sync_cam.py
@@ -2,12 +2,29 @@ from lerobot.teleoperate import TeleoperateConfig, teleoperate
 from lerobot.robots.so101_follower import SO101FollowerConfig
 from lerobot.teleoperators.so101_leader import SO101LeaderConfig
  
-# 创建机器人配置（follower）
+from lerobot.cameras.opencv.configuration_opencv import OpenCVCameraConfig
+ 
+# 添加摄像头配置
+cameras = {
+    "front": OpenCVCameraConfig(
+        # type="opencv",
+        index_or_path=0,
+        width=640,
+        height=480,
+        fps=30
+    )
+}
+ 
 robot_config = SO101FollowerConfig(
     port="/dev/arm_left_follower",
-    id="left_follower"
+    id="left_follower",
+    cameras=cameras,  # 添加摄像头
+    disable_torque_on_disconnect=True,
+    max_relative_target=None,
+    use_degrees=False
 )
  
+
 # 创建远程操作配置（leader）
 teleop_config = SO101LeaderConfig(
     port="/dev/arm_left_leader",
@@ -20,8 +37,10 @@ teleoperate_config = TeleoperateConfig(
     robot=robot_config,
     fps=60,  # 控制频率
     display_data=True,  # 是否显示数据
-    teleop_time_s=None  # 运行时间，None 表示无限运行
+    teleop_time_s=300  # 运行5分钟
 )
+ 
+
  
 # 执行远程操作
 teleoperate(teleoperate_config)
